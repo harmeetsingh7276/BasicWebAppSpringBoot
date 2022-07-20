0214a3cfc953553b1e74bf084c71bdb2d2cfe70d
harmeet <harmeetsingh7276@gmail.com>
2022-07-20 11:07:39 +0530

    Using class ModelAndView

--- BasicWebAppSpring/src/main/java/com/example/demo/HomeController.java
+++ BasicWebAppSpring/src/main/java/com/example/demo/HomeController.java
@@ -6,19 +6,21 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping("home")
-	public String home(@RequestParam("name")String myName, HttpSession session) {
+	public ModelAndView home(@RequestParam("name")String myName) {
 		// remember this name parameter should be the same as in the request parameter
 		// if you want to change the parameter name in method from the parameter you
 		// pass in the request then use RequestParam("paramName") in the method before
 		// declaring name variable and vice versa
-		System.out.println("In Home Method");
-		System.out.println("Hi " + myName);
-		session.setAttribute("name", myName);
-		return "home";
+		
+		ModelAndView mv=new ModelAndView();
+		mv.addObject("name", myName);
+		mv.setViewName("home");
+		return mv;
 	}
 }
