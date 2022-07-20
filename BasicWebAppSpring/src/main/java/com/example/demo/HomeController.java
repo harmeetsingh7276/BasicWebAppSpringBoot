487b7111333067bd7f553bb065a9056422c01655
harmeet <harmeetsingh7276@gmail.com>
2022-07-20 11:00:34 +0530

    removing the declarations and using it in a SpringBoot way

--- BasicWebAppSpring/src/main/java/com/example/demo/HomeController.java
+++ BasicWebAppSpring/src/main/java/com/example/demo/HomeController.java
@@ -10,10 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
 	@RequestMapping("home")
-	public String home(HttpServletRequest req) {
-		HttpSession session = req.getSession();
+	public String home(String name,HttpSession session) {
+		//remember this name parameter should be the same as in the request parameter 
 		System.out.println("In Home Method");
-		String name = req.getParameter("name");
 		System.out.println("Hi " + name);
 		session.setAttribute("name", name);
 		return "home";
