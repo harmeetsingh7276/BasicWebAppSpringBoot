dc01a57a4d9244b2e50392f827b6611b3066c11a
harmeet <harmeetsingh7276@gmail.com>
2022-07-20 11:04:14 +0530

    If method parameter name and request parameter name is different

--- BasicWebAppSpring/src/main/java/com/example/demo/HomeController.java
+++ BasicWebAppSpring/src/main/java/com/example/demo/HomeController.java
@@ -5,16 +5,20 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping("home")
-	public String home(String name,HttpSession session) {
-		//remember this name parameter should be the same as in the request parameter 
+	public String home(@RequestParam("name")String myName, HttpSession session) {
+		// remember this name parameter should be the same as in the request parameter
+		// if you want to change the parameter name in method from the parameter you
+		// pass in the request then use RequestParam("paramName") in the method before
+		// declaring name variable and vice versa
 		System.out.println("In Home Method");
-		System.out.println("Hi " + name);
-		session.setAttribute("name", name);
+		System.out.println("Hi " + myName);
+		session.setAttribute("name", myName);
 		return "home";
 	}
 }
